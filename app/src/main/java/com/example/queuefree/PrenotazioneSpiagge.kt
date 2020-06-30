9a64b061f6072877cb4875dba414d36d5b815410
alessandroimmordino <aleimmordino@gmail.com>
2020-06-30 12:22:29 +0200

    Prenotazione Spiagge

--- app/src/main/java/com/example/queuefree/PrenotazioneSpiagge.kt
+++ app/src/main/java/com/example/queuefree/PrenotazioneSpiagge.kt
@@ -1,50 +1,59 @@
 package com.example.queuefree
 
-
-
-
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
+import android.widget.SearchView
 import androidx.fragment.app.FragmentActivity
-import com.google.android.gms.common.api.Status
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.libraries.places.api.model.Place
-import com.google.android.libraries.places.widget.AutocompleteSupportFragment
-import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
-import java.util.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import java.io.IOException
+
+class PrenotazioneSpiagge : FragmentActivity(), OnMapReadyCallback {
 
 
-class PrenotazioneSpiagge : FragmentActivity(), OnMapReadyCallback,PlaceSelectionListener {
+    lateinit var map: GoogleMap
+    lateinit var mapFragment: SupportMapFragment
+    lateinit var searchView: SearchView
 
 
-    private var mMap: GoogleMap? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.maps_activity)
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment?
+        setContentView(R.layout.activity_main)
+        searchView = findViewById(R.id.sv_location)
+        mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        searchView!!.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                val location = searchView!!.getQuery().toString()
+                var addressList: List<Address>? = null
+                if (location != null || location != " ") {
+                    val geocoder = Geocoder(this@PrenotazioneSpiagge)
+                    try {
+                        addressList = geocoder.getFromLocationName(location, 1)
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                    val address = addressList!![0]
+                    val latlng = LatLng(address.latitude, address.longitude)
+                    map!!.addMarker(MarkerOptions().position(latlng).title(location))
+                    map!!.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng, 10f))
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
         mapFragment!!.getMapAsync(this)
-
-        val autocompleteFragment =
-            supportFragmentManager.findFragmentById(R.id.autocomplete_fragment) as AutocompleteSupportFragment?
-
-
-        autocompleteFragment!!.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME))
-
-        autocompleteFragment!!.setOnPlaceSelectedListener(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-
-    }
-
-    override fun onPlaceSelected(p0: Place) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onError(p0: Status) {
-        TODO("Not yet implemented")
+        map = googleMap
     }
 }
