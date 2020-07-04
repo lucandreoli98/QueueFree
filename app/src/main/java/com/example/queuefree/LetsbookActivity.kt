625a098f3db30fdb2066ab5099b1a3e4223d9bc4
lucandreoli98 <s4493578@studenti.unige.it>
2020-07-04 15:09:51 +0200

    New chapter: The booking zone

--- app/src/main/java/com/example/queuefree/LetsbookActivity.kt
+++ app/src/main/java/com/example/queuefree/LetsbookActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 
-class Letsbook: AppCompatActivity() {
+class LetsbookActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_letsbook)
 
+        supportFragmentManager.beginTransaction().replace(R.id.activity_letsbook, LetsbookFragment()).commit()
 
     }
 }
