df0f1933c72e9ada9f4f42a0a260076ae38ac5d0
lucandreoli98 <s4493578@studenti.unige.it>
2020-07-04 14:40:26 +0200

    New chapter: The booking zone

---
+++ app/src/main/java/com/example/queuefree/Letsbook.kt
@@ -0,0 +1,13 @@
+package com.example.queuefree
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Letsbook: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+
+    }
+}
