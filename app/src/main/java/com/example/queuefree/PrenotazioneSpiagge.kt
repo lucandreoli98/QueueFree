51044106a4405a6e49941300dfacbf078cd04eb3
Enrico Fiasche <enrico.fiasche@gmail.com>
2020-07-04 17:21:18 +0200

    FINE DELLE FINI 4 LUGLIO 2020

--- app/src/main/java/com/example/queuefree/PrenotazioneSpiagge.kt
+++ app/src/main/java/com/example/queuefree/PrenotazioneSpiagge.kt
@@ -96,7 +96,7 @@ class PrenotazioneSpiagge : FragmentActivity(), OnMapReadyCallback,GoogleMap.OnM
         }
         passDialogView.prenota.setOnClickListener {
             val intent = Intent(this,LetsbookActivity::class.java)
-            //intent.putExtra("email",p0!!.snippet)
+            intent.putExtra("email",p0!!.snippet)
             startActivity(intent)
 
         }
