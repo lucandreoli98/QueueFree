922118e6422d82c9cee491eb919a98f8a8bdef31
alessandroimmordino <aleimmordino@gmail.com>
2020-07-05 16:41:48 +0200

    Ho creato una dashboard così che dal lato azienda non si abbia solo una pagina

--- app/src/main/java/com/example/queuefree/MainActivity.kt
+++ app/src/main/java/com/example/queuefree/MainActivity.kt
@@ -9,6 +9,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+
         supportFragmentManager.beginTransaction().replace(R.id.login_fragment, LoginFragment()).commit()
     }
 }
