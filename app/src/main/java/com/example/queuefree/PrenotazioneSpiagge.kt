12c39d06d2bfa8469a8655c53c30eae24fed69bc
lucandreoli98 <s4493578@studenti.unige.it>
2020-07-05 15:21:48 +0200

    bugs

--- app/src/main/java/com/example/queuefree/PrenotazioneSpiagge.kt
+++ app/src/main/java/com/example/queuefree/PrenotazioneSpiagge.kt
@@ -2,12 +2,9 @@ package com.example.queuefree
 
 import android.app.AlertDialog
 import android.content.Intent
-import android.graphics.BitmapFactory
 import android.location.Geocoder
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import androidx.fragment.app.FragmentActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -21,94 +18,75 @@ import kotlinx.android.synthetic.main.custom_info_window.view.*
 
 class PrenotazioneSpiagge : FragmentActivity(), OnMapReadyCallback,GoogleMap.OnMarkerClickListener{
 
-
     lateinit var map: GoogleMap
-    lateinit var mapFragment: SupportMapFragment
-
-
-
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.maps_activity)
-        mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-
-
-
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
+
         map = googleMap
         map.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(44.4222, 8.9052), 8f))
 
         val geo = Geocoder(this)
 
-
-        val database:FirebaseDatabaseHelper= FirebaseDatabaseHelper()
-
-
-
-        database.readFirmsandtakeAdress(object : FirebaseDatabaseHelper.DataStatusFirm {
+        FirebaseDatabaseHelper().readFirmsandtakeAdress(object : FirebaseDatabaseHelper.DataStatusFirm {
             override fun DataisLoadedFirm(firm: Firm){
-
 
                 geo.getFromLocationName(firm.location,1)
 
-                val latlng =LatLng( geo.getFromLocationName(firm.location,1).get(0).latitude, geo.getFromLocationName(
+                val latlng = LatLng( geo.getFromLocationName(firm.location,1)[0].latitude, geo.getFromLocationName(
                     firm.location,1)[0].longitude)
 
-                if(intent.getStringExtra("tipo") == "Spiaggia") {
 
-                    map.addMarker(MarkerOptions().position(latlng).title(firm.nomeazienza).snippet(firm.email).icon(BitmapDescriptorFactory.fromResource(R.drawable.ombrellone)))
-                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng,10f))
+                when(intent.getStringExtra("tipo")) {
+                    "Spiaggia" -> {
+                        map.addMarker(MarkerOptions().position(latlng).title(firm.nomeazienza).snippet(firm.email).icon(BitmapDescriptorFactory.fromResource(R.drawable.ombrellone)))
+                        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 10f))
+                    }
 
+                    "Museo" -> {
+                        map.addMarker(MarkerOptions().position(latlng).title(firm.nomeazienza).snippet(firm.email).icon(BitmapDescriptorFactory.fromResource(R.drawable.museo)))
+                        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 10f))
+                    }
+
+                    "Biblioteca" -> {
+                        map.addMarker(MarkerOptions().position(latlng).title(firm.nomeazienza).snippet(firm.email).icon(BitmapDescriptorFactory.fromResource(R.drawable.libro)))
+                        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 10f))
+                    }
                 }
-                else if(intent.getStringExtra("tipo") == "Museo") {
-                    map.addMarker(MarkerOptions().position(latlng).title(firm.nomeazienza).snippet(firm.email).icon(BitmapDescriptorFactory.fromResource(R.drawable.museo)))
-                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng,10f))
-
-                }
-                else if(intent.getStringExtra("tipo") == "Biblioteca") {
-                    map.addMarker(MarkerOptions().position(latlng).title(firm.nomeazienza).snippet(firm.email).icon(BitmapDescriptorFactory.fromResource(R.drawable.libro)))
-                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng,10f))
-
-                }
-
-
             }
-
         },intent.getStringExtra("tipo")!!)
 
         map.setOnMarkerClickListener(this)
 
-
-
     }
 
     override fun onMarkerClick(p0: Marker?): Boolean {
+
         val passDialogView = LayoutInflater.from(this).inflate(R.layout.custom_info_window, null)
+        val mBuilder = AlertDialog.Builder(this).setView(passDialogView)
+        val alertDialog = mBuilder.show()
+
         if (p0 != null) {
             passDialogView.titlefirm.text = p0.title
             passDialogView.info_firm.text=p0.snippet
         }
+
         passDialogView.prenota.setOnClickListener {
             val intent = Intent(this,LetsbookActivity::class.java)
-            Log.e("SNIPPPPPPPPPPPPPPPPETTTTTTT",p0!!.snippet)
-            intent.putExtra("email",p0!!.snippet)
+            if (p0 != null)
+                intent.putExtra("email", p0.snippet)
+
+            alertDialog.dismiss()
             startActivity(intent)
 
         }
 
-        val mBuilder = AlertDialog.Builder(this).setView(passDialogView)
-        val alertDialog = mBuilder.show()
-
-
-
-
-    return false
+        return false
     }
-
 }
