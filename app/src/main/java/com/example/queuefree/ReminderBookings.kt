8b98438ff4c3d5ab9ff4aa5910310829f1fc8f5e
alessandroimmordino <aleimmordino@gmail.com>
2020-07-12 13:55:43 +0200

    Notification, le basi

---
+++ app/src/main/java/com/example/queuefree/ReminderBookings.kt
@@ -0,0 +1,115 @@
+package com.example.queuefree
+
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import java.util.*
+import kotlin.collections.HashMap
+
+class ReminderBookings :Service() {
+
+    private val database = FirebaseDatabaseHelper()
+
+
+
+
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    @Override
+    override fun onCreate() {
+        startTimer()
+
+
+    }
+
+
+
+    private fun startTimer() {
+        val task: TimerTask = object : TimerTask() {
+            override fun run() {
+
+                val currentDate = Date(Date().year,Date().month,Date().day)
+                database.readAllFirmFromDB(object:FirebaseDatabaseHelper.DataStatusHashFirm{
+                    override fun dataisLoadedFirm(firms: HashMap<String, Firm>) {
+                        database.readBookingUser(firms,object:FirebaseDatabaseHelper.DataBookingUser{
+                            override fun BookingUserisLoaded(bookingUser: ArrayList<BookingUser>) {
+                                for (BookingsFirm in bookingUser){
+                                    for(Bookings in BookingsFirm.bookings){
+                                        val date=Date(Bookings.yy.toInt(),Bookings.mm.toInt(),Bookings.dd.toInt())
+
+                                        Log.d("NOTIFICA","${BookingsFirm.firm.nomeazienza} : ${Bookings.dd}/${Bookings.mm}/${Bookings.yy}, current date : ${currentDate.time}")
+                                        if(date.day==currentDate.day || date.month==currentDate.month || date.year==currentDate.year){
+                                            sendNotification(BookingsFirm.firm.nomeazienza)
+
+                                        }
+                                        else
+                                            sendNotification("Non ci sono prenotazioni")
+                                    }
+                                }
+                            }
+
+                        })
+
+                    }
+
+                })
+
+
+
+
+
+            }
+
+        }
+
+       var timer = Timer(true)
+        val delay = 1000 * 10 // 10 seconds
+
+        val interval = 1000 * 60  // 1 hour
+
+        timer.schedule(task, delay.toLong(), interval.toLong())
+
+    }
+
+
+    private fun sendNotification(text: String) {
+        // create the intent for the notification
+        val notificationIntent = Intent(this, MainActivity::class.java)
+            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+
+        // create the pending intent
+        val flags = PendingIntent.FLAG_UPDATE_CURRENT
+        val pendingIntent =
+            PendingIntent.getActivity(this, 0, notificationIntent, flags)
+
+        // create the variables for the notification
+        val icon: Int = R.drawable.ic_alarm_24
+        val tickerText: CharSequence = "Oggi hai il seguente evento prenotato"
+        val contentTitle = getText(R.string.app_name)
+        val contentText: CharSequence = text
+
+        // create the notification and set its data
+        val notification: Notification = Notification.Builder(this)
+            .setSmallIcon(icon)
+            .setTicker(tickerText)
+            .setContentTitle(contentTitle)
+            .setContentText(contentText)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .build()
+
+        // display the notification
+        val manager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val NOTIFICATION_ID = 1
+        manager.notify(NOTIFICATION_ID, notification)
+    }
+}
